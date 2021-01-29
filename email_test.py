66e9ae77ea760e1a55e5dca09a7f935fef50497c
stanger <4blokadanhilla@gmail.com>
2021-01-29 14:40:44 +0300

    add_homework

--- email_test.py
+++ email_test.py
@@ -1,17 +1,15 @@
-import pytest
-
-from fixture import log
+from conftest import *
 from func import *
 
+
+@pytest.mark.usefixtures("log")
 @pytest.mark.parametrize('email, result', (
-    ["test@test.ru", True],
-    ["w@w.com", True],
-    ["123QWE@mmm.mmm", True],
-    ["test@test.", False],
-    ["w@.", False],
-    ["@tt.", False]
-                        ))
+        ["test@test.ru", True],
+        ["w@w.com", True],
+        ["123QWE@mmm.mmm", True],
+        ["test@test.", False],
+        ["w@.", False],
+        ["@tt.", False]
+))
 def test_valid_email(email, result):
     assert valid_email(email) == result
-    # log(result)
-
